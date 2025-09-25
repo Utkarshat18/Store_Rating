02395aa0bfa97f20afdd86d991ef699f0fd1a83c
Utkarsha Turambekar <utkarshat200318@gmail.com>
2025-09-25 12:46:54 +0530

    Created Backend server

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express= require('express');
+const app=express();
+require('dotenv').config();
+
+const PORT=process.env.PORT || 5000;
+
+app.get('/',(req,res)=>{
+    console.log("API is running");
+    res.send("API is running on PORT "+PORT);
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server running on PORT ${PORT}`);
+}
+)
+
