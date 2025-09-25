a653014de58aeedd02779d7d1cbc7bd0ba507bb0
Utkarsha Turambekar <utkarshat200318@gmail.com>
2025-09-25 17:28:38 +0530

    Backend structure completed

--- backend/Routes/ProductRouter.js
+++ backend/Routes/ProductRouter.js
@@ -2,6 +2,7 @@ const ensurevalidation=require('../Middlewares/AuthCheck');
 const router=require('express').Router();
 
 router.get('/',ensurevalidation,(req,res)=>{
+    console.log("Products API is called",req.user);
     res.status(200)
     .json([{name:"Product 1",price:100,description:"This is product 1"},
     {name:"Product 2",price:200,description:"This is product 2"},
